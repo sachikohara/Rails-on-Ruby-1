680f96e4e851560d2be63211f00832ba76577b0a
sa ha <saha@sanoMacBook-Air.local>
2023-12-07 00:41:08 +0900

    課題再提出(PRを作成）

--- config/application.rb
+++ config/application.rb
@@ -6,11 +6,12 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module TASK
+module Myapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
+    config.time_zone = 'Tokyo'
+    Time::DATE_FORMATS[:default] = "%Y/%m/%d %H:%M"
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
