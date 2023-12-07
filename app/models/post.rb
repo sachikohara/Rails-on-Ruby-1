4eae11a5a8746ead4f68850cf641d1bef0796a6c
sa ha <saha@sanoMacBook-Air.local>
2023-12-07 12:51:13 +0900

    バリデーション（終了日が開始日より前を追加）

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,9 +5,13 @@ class Post < ApplicationRecord
   
   validates :memo, length: { maximum: 500 }, allow_blank: true
 
-  def start_end_check
-    if start_date && end_date && start_date > end_date
-        errors.add(:end_date, "は開始日より前の日を設定はだめ") 
-    end
+  validate :end_date_after_start_date
+
+  private
+
+  def end_date_after_start_date
+    return if end_date.blank? || start_date.blank?
+
+    errors.add(:end_date, "は開始日より後である必要があります") if end_date < start_date
   end
 end
